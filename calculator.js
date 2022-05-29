ae185f2efdf0e3fb6ed6e22047738b721146481c
Baris <baslan374@gmail.com>
2022-05-29 21:44:42 +0300

    Add getDisplayNumber function

--- calculator.js
+++ calculator.js
@@ -28,9 +28,20 @@ class Calculator {
     this.currentOperand = '';
 }
 
+    getDisplayNumber(number) {
+        const floatNumber = parseFloat(number)
+        if (isNaN(floatNumber)) return ""
+        return floatNumber.toLocaleString("en")
+    }
+
     updateDisplay(){
-    this.currentOperandTextElement.innerText = this.currentOperand;
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+    if(this.operation != null) {
+        this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    }
+    else {
+        this.previousOperandTextElement.innerText = ""
+    }
 }
 
     compute(){
